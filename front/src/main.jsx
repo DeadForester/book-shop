b55e6c10d83322888087f12cfe88be5efa96bd56
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-05-14 22:28:32 +0300

    refactor: use correct folders

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.jsx'
 import {ThemeProvider} from '@mui/material';
-import {theme} from "./components/Theme.js";
+import {theme} from "./shared/components/Theme.js";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
