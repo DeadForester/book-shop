a8aefdf3af66e177b51aa3e4a7581694f28babfd
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-05-14 23:26:18 +0300

    fix: fix dependency scan

--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -1,14 +1,14 @@
 import Books from "../pages/Books.jsx";
 import Error from "../pages/Error.jsx";
 
-export const privateRoutes = [{ path: "/panel", element: <></> }];
+export const privateRoutes = [{ path: "/panel", element: null }];
 
 export const publicRoutes = [
     {
         path: "/books",
         element: Books,
     },
-    { path: "/books:id", element: <></> },
-    { path: "/login", element: <></> },
+    { path: "/books:id", element: null },
+    { path: "/login", element: null },
     { path: "*", element: Error },
 ];
