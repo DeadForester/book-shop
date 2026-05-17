35963eb6c26ea40cfe116d8797800654205856e1
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-05-18 00:11:41 +0300

    feat: add tabs for different charts printing

--- front/src/pages/Dashboard.jsx
+++ front/src/pages/Dashboard.jsx
@@ -1,8 +1,33 @@
-const Dashboard = () => {
-    return (
-        <div>
+import {Box, Tab, Tabs} from "@mui/material";
+import PhoneIcon from '@mui/icons-material/Phone';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import {useState} from "react";
 
-        </div>
+const Dashboard = () => {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{
+                borderBottom: 1,
+                borderColor: 'divider'
+            }}>
+                <Tabs value={value}
+                      onChange={handleChange}
+                      aria-label="dashboard tabs"
+                      variant="fullWidth"
+                >
+                    <Tab icon={<PhoneIcon />} label="RECENTS" />
+                    <Tab icon={<FavoriteIcon />} label="FAVORITES" />
+                    <Tab icon={<PersonPinIcon />} label="NEARBY" />
+                </Tabs>
+            </Box>
+        </Box>
     );
 };
 
