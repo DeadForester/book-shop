0d832a8d32497620a8898a934b40f7fd4e8df6ae
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-05-21 00:17:05 +0300

    fix: sync state for use it with localstorage

--- front/src/shared/context/AuthProvider.jsx
+++ front/src/shared/context/AuthProvider.jsx
@@ -1,13 +1,21 @@
-import {useMemo, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {AuthContext} from "../../context/auth.js";
 
 const AuthProvider = ({children}) => {
     const [isAuth, setIsAuth] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
     const value= useMemo(() => ({
         isAuth,
-        setIsAuth
-    }), [isAuth]);
+        setIsAuth,
+        isLoading
+    }), [isAuth, isLoading]);
+
+    useEffect(() => {
+        const saved = localStorage.getItem('auth');
+        setIsAuth(saved === 'true');
+        setIsLoading(false);
+    }, []);
 
     return (
         <AuthContext.Provider value={value}>
