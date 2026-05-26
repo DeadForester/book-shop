153da8a5b39ad50d217202c62b34f5516dae1bf7
Mathew <mattew1997@yandex.ru>
2026-05-26 12:52:52 +0300

    Add in container redis-image.
    add:
    - Add in docker-compose.yaml redis-image;
    - Add Dockerfile for frontend;
    - Add redis dependencies in pom.xml;
    
    update:
    - Update docker-compose.yaml. Add for all image container_name;
    - Update vite.config.js. Add host for correct work frontend;
    - Update application.yaml. Add Server_port for dynamic port in backend.

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,8 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: '0.0.0.0', //Для Network - временная заглушка
   },
   preview: {
     port: 8080,
+    host: '0.0.0.0', //Для Network - временная заглушка
   }
 })
