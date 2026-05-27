09deb83905f7d6b5098289ef0c7b38a31d336510
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-05-27 17:32:10 +0300

    feat: connect history page to app

--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -3,14 +3,15 @@ import BookIdPage from "../pages/BookIdPage.jsx";
 import Dashboard from "../pages/Dashboard.jsx";
 import Login from "../pages/Login.jsx";
 import Register from "../pages/Register.jsx";
+import OrderHistory from "../pages/OrderHistory.jsx";
 
-export const privateRoutes = [{ path: "/panel", element: Dashboard }];
+export const privateRoutes = [
+    { path: "/panel", element: Dashboard },
+    { path: "/history", element: OrderHistory },
+];
 
 export const publicRoutes = [
-    {
-        path: "/books",
-        element: Books,
-    },
+    { path: "/books",element: Books },
     { path: "/books/:id", element: BookIdPage },
     { path: "/login", element: Login },
     { path: "/register", element: Register },
