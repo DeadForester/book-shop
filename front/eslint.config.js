8d48a3ef848da8e70984219065378081da74f7d1
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-05-27 23:23:59 +0300

    feat: add prettier to project

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -5,17 +5,25 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{js,jsx}'],
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            globals: globals.browser,
+            parserOptions: { ecmaFeatures: { jsx: true } },
+        },
+        rules: {
+            indent: 'off',
+            semi: 'off',
+            quotes: 'off',
+            'comma-dangle': 'off',
+            'no-trailing-spaces': 'off',
+            'linebreak-style': 'off',
+        },
     },
-  },
-])
+]);
