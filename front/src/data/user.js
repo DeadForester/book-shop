7c1ca23374729963a31086ce5752b41f39248d34
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-05-27 23:09:19 +0300

    feat: add user mock data

---
+++ front/src/data/user.js
@@ -0,0 +1,8 @@
+export const mockUser = {
+    email: 'alex.ivanov@bookshop.ru',
+    name: 'Алексей Иванов',
+    isAdmin: false,
+    phone: '+7 (900) 123-45-67',
+    joinDate: '12.05.2023',
+    address: 'г. Москва, ул. Тверская, д. 15, кв. 42'
+};
