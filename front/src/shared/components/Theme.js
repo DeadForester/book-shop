b2f57b48dfd4afa0cd01b36d641a9d4b1a7647e5
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-05-29 00:43:54 +0300

    feat: add theme colors

--- front/src/shared/components/Theme.js
+++ front/src/shared/components/Theme.js
@@ -1,12 +1,18 @@
-import {createTheme} from '@mui/material';
+import { createTheme } from '@mui/material';
 
 export const theme = createTheme({
     palette: {
         primary: {
-            main: '#61dafb'
+            main: '#61dafb',
         },
         secondary: {
-            main: '#da61bf'
-        }
-    }
-})
+            main: '#da61bf',
+        },
+        palette: {
+            bookCard: {
+                even: '#ffcdd2',
+                odd: '#e1bee7',
+            },
+        },
+    },
+});
