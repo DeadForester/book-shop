6d8053b3b239f678703d4683f0d89b9bc6a20f4f
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-05-30 00:29:04 +0300

    feat: add routes for future admin pages

--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -1,20 +1,21 @@
-import Books from "../pages/Books.jsx";
-import BookIdPage from "../pages/BookIdPage.jsx";
-import Dashboard from "../pages/Dashboard.jsx";
-import Login from "../pages/Login.jsx";
-import Register from "../pages/Register.jsx";
-import OrderHistory from "../pages/OrderHistory.jsx";
-import Profile from "../pages/Profile.jsx";
+import Books from '../pages/Books.jsx';
+import BookIdPage from '../pages/BookIdPage.jsx';
+import Dashboard from '../pages/Dashboard.jsx';
+import Login from '../pages/Login.jsx';
+import Register from '../pages/Register.jsx';
+import OrderHistory from '../pages/OrderHistory.jsx';
+import Profile from '../pages/Profile.jsx';
 
-export const privateRoutes = [
-    { path: "/panel", element: Dashboard },
-    { path: "/orders", element: OrderHistory },
-    { path: "/profile", element: Profile },
+export const adminRoutes = [{ path: '/panel', element: Dashboard }];
+
+export const userRoutes = [
+    { path: '/orders', element: OrderHistory },
+    { path: '/profile', element: Profile },
 ];
 
 export const publicRoutes = [
-    { path: "/books",element: Books },
-    { path: "/books/:id", element: BookIdPage },
-    { path: "/login", element: Login },
-    { path: "/register", element: Register },
+    { path: '/books', element: Books },
+    { path: '/books/:id', element: BookIdPage },
+    { path: '/login', element: Login },
+    { path: '/register', element: Register },
 ];
--- front/src/shared/components/AppRouter.jsx
+++ front/src/shared/components/AppRouter.jsx
@@ -1,38 +1,43 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import {privateRoutes, publicRoutes} from "../../router/routes.js";
-import Loader from "../../components/UI/loader/Loader.jsx";
-import Books from "../../pages/Books.jsx";
-import {useAuthContext} from "../../hooks/useAuthContext.js";
-import ProtectedRoute from "./ProtectedRoute.jsx";
-import Error from "../../pages/Error.jsx";
+import { adminRoutes, publicRoutes, userRoutes } from '../../router/routes.js';
+import Loader from '../../components/UI/loader/Loader.jsx';
+import Books from '../../pages/Books.jsx';
+import { useAuthContext } from '../../hooks/useAuthContext.js';
+import ProtectedRoute from './ProtectedRoute.jsx';
+import Error from '../../pages/Error.jsx';
 
 const AppRouter = () => {
-    const {isAuth, isLoading} = useAuthContext();
+    const { isAuth, isLoading } = useAuthContext();
 
     if (isLoading) {
-        return <Loader/>
+        return <Loader />;
     }
 
     return (
         <Routes>
-            <Route path="/" element={<Books/>}/>
-            {publicRoutes.map((route, index) =>
-                <Route
-                    key={`public-${index}`}
-                    path={route.path}
-                    element={<route.element/>}
-                />
-            )}
+            <Route path="/" element={<Books />} />
+            {publicRoutes.map((route, index) => (
+                <Route key={`public-${index}`} path={route.path} element={<route.element />} />
+            ))}
 
             <Route element={<ProtectedRoute />}>
-                {isAuth && privateRoutes.map((route, index) =>
-                    <Route
-                        key={`private-${index}`}
-                        path={route.path}
-                        element={<route.element/>}
-                    />
-                )}
+                {isAuth &&
+                    userRoutes.map((route, index) => (
+                        <Route
+                            key={`private-${index}`}
+                            path={route.path}
+                            element={<route.element />}
+                        />
+                    ))}
+                {isAuth &&
+                    adminRoutes.map((route, index) => (
+                        <Route
+                            key={`private-${index}`}
+                            path={route.path}
+                            element={<route.element />}
+                        />
+                    ))}
             </Route>
 
             <Route path="*" element={<Error />} />
