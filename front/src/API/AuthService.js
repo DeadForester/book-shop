1e634e61689a11115f85116e7abedf56d18b2eee
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-06-03 12:49:32 +0300

    fix: include cookie to requests

--- front/src/API/AuthService.js
+++ front/src/API/AuthService.js
@@ -2,7 +2,16 @@ import axios from 'axios';
 
 export default class AuthService {
     static async login(email, password) {
-        return await axios.post('http://localhost:8080/api/v1/login', { email, password });
+        return await axios.post(
+            'http://localhost:8080/api/v1/login',
+            { email, password },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                withCredentials: true,
+            }
+        );
     }
 
     static async registration(email, password) {
--- front/src/API/UserService.js
+++ front/src/API/UserService.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export default class UserService {
     static async getUserById(id) {
-        return await axios.get(`http://localhost:8080/api/v1/user/${id}`);
+        return await axios.get(`http://localhost:8080/api/v1/user/${id}`, {
+            withCredentials: true,
+        });
     }
 }
