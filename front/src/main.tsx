1a54540f0bca403f0d21c0b75947f18446d001dc
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-06-08 01:57:01 +0300

    fix: resolve error in main

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -8,7 +8,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '@/store/store.ts';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+    throw new Error('Root element not found. Check your index.html');
+}
+
+createRoot(rootElement).render(
     <StrictMode>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
