a7a6ef0f9013f2292681bf0cea0602f000652055
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-06-09 02:22:19 +0300

    remove: remove old context

--- front/src/context/auth.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-import { AuthContextType } from '@/shared/types/AuthContextType.ts';
-
-export const AuthContext = createContext<AuthContextType | null>(null);
--- front/src/hooks/useBasketContext.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react';
-import { BasketContext } from '../context/basket.ts';
-
-export const useBasketContext = () => useContext(BasketContext);
