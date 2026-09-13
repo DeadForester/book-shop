41fad3025ace510de42ce1dbb2d45ca107675c57
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-09-13 20:39:56 +0300

    feat: install storybook

--- front/eslint.config.ts
+++ front/eslint.config.ts
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -6,26 +9,23 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig([
-    globalIgnores(['dist']),
-    {
-        files: ['**/*.{ts,tsx}'],
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactHooks.configs.flat.recommended,
-            reactRefresh.configs.vite,
-        ],
-        plugins: {
-            'simple-import-sort': simpleImportSort,
-        },
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
-        },
-        rules: {
-            'simple-import-sort/imports': 'error',
-            'simple-import-sort/exports': 'error',
-        },
+export default defineConfig([globalIgnores(['dist']), {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs.flat.recommended,
+        reactRefresh.configs.vite,
+    ],
+    plugins: {
+        'simple-import-sort': simpleImportSort,
     },
-]);
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+    },
+    rules: {
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+    },
+}, ...storybook.configs["flat/recommended"]]);
